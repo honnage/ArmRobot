78abba1c91576ffe7f2ac57606bc555f5875aa7e
honnage <hon_da-1998@hotmail.com>
2021-07-08 21:08:17 +0700

    Update code

---
+++ Code/TestLed_start.py
@@ -0,0 +1,34 @@
+from __future__ import division
+from threading import Thread
+import RPi.GPIO as GPIO
+import multiprocessing as mp
+import Adafruit_PCA9685
+import cv2
+import numpy as np
+import dlib
+import math 
+import time
+import os
+#import drivers
+print("Start Project ....")
+impoxrt Ultrasonict
+ 
+print(GPIO.VERSION)
+GPIO.setmode(GPIO.BCM)
+ledStart = 18
+ledWork = 15
+
+GPIO.setup(ledStart, GPIO.OUT)
+GPIO.setup(ledWork, GPIO.OUT)
+
+GPIO.output(ledStart, GPIO.HIGH)
+print("\nLED Start... ON")
+
+while True:	
+	GPIO.output(ledWork, GPIO.HIGH)
+	print("\nLED Working... ON")
+	time.sleep(1)
+	
+	GPIO.output(ledWork, GPIO.LOW)
+	print("LED Working... OFF")
+	time.sleep(3)
